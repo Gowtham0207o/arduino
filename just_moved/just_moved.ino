30c23894dba852b8102fa82f13bc2576d7201baf
Gowtham Ravi <gowthamravi032@gmail.com>
2022-05-30 21:50:56 +0530

    the code is moved to a function

--- just_moved/just_moved.ino
+++ just_moved/just_moved.ino
@@ -15,18 +15,17 @@ AF_DCMotor motor2(4, MOTOR12_8KHZ);
 
 void setup() {
  
-  motor1.setSpeed(200);
-  motor2.setSpeed(200);
-  motor3.setSpeed(200);
-  motor4.setSpeed(200);
+  motor1.setSpeed(250);
+  motor2.setSpeed(250);
+  motor3.setSpeed(250);
+  motor4.setSpeed(250);
 
   Serial.begin(9600);
   
 }
-
-void loop(){
-
-      motor1.run(FORWARD);
+void basic_move(){
+  
+        motor1.run(FORWARD);
       motor2.run(FORWARD);   
       motor3.run(FORWARD);
       motor4.run(FORWARD);
@@ -46,7 +45,13 @@ void loop(){
     motor3.run(RELEASE);
     motor4.run(RELEASE);
     delay(1000);
+  
+  }
 
+
+void loop(){
+
+basic_move();
    
     //turn left
     motor1.run(BACKWARD);
@@ -54,28 +59,7 @@ void loop(){
     motor4.run(BACKWARD);
     motor3.run(FORWARD);
     delay(1000);
-      motor1.run(FORWARD);
-      motor2.run(FORWARD);   
-      motor3.run(FORWARD);
-      motor4.run(FORWARD);
-      delay(1000);
-      
-
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    motor3.run(RELEASE);
-    motor4.run(RELEASE);
-    delay(800);
-   motor1.run(BACKWARD);  
-   motor2.run(BACKWARD);  
-   motor3.run(BACKWARD); 
-   motor4.run(BACKWARD);
-   delay(1000);
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    motor3.run(RELEASE);
-    motor4.run(RELEASE);
-    delay(800);
+     basic_move();
 
     //turn right
     motor1.run(FORWARD);
@@ -88,28 +72,7 @@ void loop(){
     motor4.run(FORWARD);
     motor3.run(BACKWARD);
     delay(1000);
-      motor1.run(FORWARD);
-      motor2.run(FORWARD);   
-      motor3.run(FORWARD);
-      motor4.run(FORWARD);
-      delay(1000);
-      
-
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    motor3.run(RELEASE);
-    motor4.run(RELEASE);
-    delay(800);
-   motor1.run(BACKWARD);  
-   motor2.run(BACKWARD);  
-   motor3.run(BACKWARD); 
-   motor4.run(BACKWARD);
-   delay(1000);
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    motor3.run(RELEASE);
-    motor4.run(RELEASE);
-    delay(800);
+      basic_move();
      motor1.run(BACKWARD);
     motor2.run(FORWARD);
      motor4.run(BACKWARD);
